56605f4e76bd559a5abd0b2de58044d96bb7fb4f
Veronika2007931 <113263850+Veronika2007931@users.noreply.github.com>
2024-03-02 11:38:39 +0200

    add

--- src/components/Pokemon/PokemonImfo.jsx
+++ src/components/Pokemon/PokemonImfo.jsx
@@ -7,32 +7,60 @@ export class PokemonInfo extends PureComponent{
 
 state={
     pokemon:"",
-    loading: false
-
+    isLoading: false,
+    error: null,
+    status: 'idle'
 }
 
 componentDidUpdate(prevProps){
 
     if(prevProps.pokemonName !== this.props.pokemonName){   
-        this.setState({isLoading: true, pokemon: null})
+        this.setState({status: 'pending', pokemon: null})
     fetch(`https://pokeapi.co/api/v2/pokemon/${this.props.pokemonName}`)
-    .then(res => res.json())
+    .then(res => {
+        if(!res.ok){
+          return  Promise.reject(new Error(`Покемона з іменем ${this.props.pokemonName}не знайдено`))
+            
+        }
+        return res.json()
+        })
     .then(pokemon=>{
-        this.setState({pokemon})
-        this.setState({isLoading: false})
-    })}
+        this.setState({ pokemon, error:null, status:'resolved'})})
+        .catch(error => this.setState({error, status:'rejected'}))
+       
+       
+    }
 }
 
  render(){
-    const {pokemon}=this.state
-    return(
-        <>
-       {pokemon && <div>
-            <h2>{pokemon.name}</h2>
-            <img src={pokemon.sprites.other['official-artwork'].front_default} alt="" />
-        </div>}
-        </>
-    )
+    const {pokemon, error, status}=this.state
+   
+if(status === "idle"){
+    return  <p>Введіть ім'я покемона</p>
+}
+if(status === 'pending'){
+    return <p>Loading...</p>
+}
+if (status === 'resolved'){
+   return <div>
+             <h2>{pokemon.name}</h2>
+                <img src={pokemon.sprites.other['official-artwork'].front_default} alt="" />
+            </div>
+}
+if(status === "rejected"){
+    <div>{error.message  }</div>
+}
+
+    // return(
+    //     <>{!isLoading && !pokemon && !error && <p>Введіть ім'я покемона</p>}
+    //     {isLoading && <p>Loading...</p>}
+    //     {error && <div>{error.message  }</div>}
+    //     {pokemon && <div>
+    //         <h2>{pokemon.name}</h2>
+    //         <img src={pokemon.sprites.other['official-artwork'].front_default} alt="" />
+    //     </div>}
+    //     </>
+    // )
  }
  
 }
